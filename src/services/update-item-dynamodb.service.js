62fe6cf3d587e48ee2c48704c32f6bb2ed091441
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-05 21:38:15 -0300

    fix update item service

--- src/services/update-item-dynamodb.service.js
+++ src/services/update-item-dynamodb.service.js
@@ -3,12 +3,10 @@ const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
 exports.putUserOnDatabase = async function (putItemCommand) {
     const params = {
-        ExpressionAttributeNames: putItemCommand.ExpressionAttributeNames,
         ExpressionAttributeValues: putItemCommand.ExpressionAttributeValues,
         Key: putItemCommand.Key,
         TableName: process.env.TableName,
         ReturnConsumedCapacity: "TOTAL",
-        ConditionExpression: putItemCommand.ConditionExpression
     };
 
     console.log(params);
